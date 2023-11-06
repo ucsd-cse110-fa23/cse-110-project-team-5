8e90577364d55103c3b9176f6c1fdddf5e76760f
rsa1to <ryutechncode@gmail.com>
2023-11-05 19:52:55 -0800

    moved Header and Footer classes to AppFrame

--- src/Main.java
+++ src/Main.java
@@ -93,42 +93,7 @@ import java.util.Collections;
 //     }
 // }
 
-// App Footer
-class Footer extends HBox {
-    private Button createButton;
 
-    Footer() {
-        this.setPrefSize(500, 60);
-        this.setStyle("-fx-background-color: #F0F8FF;");
-        this.setSpacing(15);
-
-        // set a default style for buttons - background color, font size, italics
-        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
-
-        createButton = new Button("New Recipe"); // text displayed on add button
-        createButton.setStyle(defaultButtonStyle); // styling the button
-
-        this.getChildren().addAll(createButton); // adding buttons to footer
-        this.setAlignment(Pos.CENTER); // aligning the buttons to center
-    }
-
-    public Button getCreateButton() {
-        return createButton;
-    }
-}
-
-// App Header
-class Header extends HBox {
-    Header() {
-        this.setPrefSize(500, 60); // Size of the header
-        this.setStyle("-fx-background-color: #F0F8FF;");
-
-        Text titleText = new Text("Recipe List"); // Text of the Header
-        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
-        this.getChildren().add(titleText);
-        this.setAlignment(Pos.CENTER); // Align the text to the Center
-    }
-}
 
 public class Main extends Application{
     @Override
