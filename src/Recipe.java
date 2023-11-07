316984b41a2c6cd78ad93c95ad27119d85a267b8
Thanh-Nhan Lam <thanhnhan.lam15@gmail.com>
2023-11-06 19:02:12 -0800

    Create the Recipe class

---
+++ src/Recipe.java
@@ -0,0 +1,27 @@
+public class Recipe {
+    String title;
+    String recipe;
+    MealType mealType;
+
+    public Recipe(MealType mealType, String title, String recipe) {
+        this.mealType = mealType;
+        this.title = title;
+        this.recipe = recipe;
+    }
+
+    public String getMealType() {
+        return this.mealType.toString();
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getRecipe() {
+        return this.recipe;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
+}
