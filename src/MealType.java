fd25efeab74242d2c081e2c5863c5e476e59a353
Thanh-Nhan Lam <thanhnhan.lam15@gmail.com>
2023-11-06 19:00:52 -0800

    Add MealType enum

---
+++ src/MealType.java
@@ -0,0 +1,11 @@
+public enum MealType {
+    BREAKFAST("Breakfast"), 
+    LUNCH("Lunch"), 
+    DINNER("Dinner");
+
+    private final String mealType;
+
+    MealType(String mealType) {
+        this.mealType = mealType;
+    }
+}
