efb63e2fa4ac72780fe03fe3b28bd67dd1e06b6a
rsa1to <ryutechncode@gmail.com>
2023-11-09 00:33:32 -0800

    copied over code from Details.java in recipe-display branch

--- src/main/java/client/Details.java
+++ src/main/java/client/Details.java
@@ -31,24 +31,40 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class Details extends VBox {
-    private TextField details;
+    private TextArea title;
+    private TextArea details;
 
     Details() {
         this.setPrefSize(500, 500);
         this.setStyle("-fx-background-color: #F0F8FF;");
 
-        details = new TextField(); // create recipe name text field
-        details.setPrefSize(380, 20); // set size of text field
+        title = new TextArea();
+        title.setPrefSize(475, 50); // set size of text field
+        // set background color, size, and style of TextArea
+        title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-background-color: #DAE5EA; -fx-border-width: 0;");
+        title.setPadding(new Insets(10, 0, 10, 0)); // adds some padding to the text field
+        title.setWrapText(true); // Enable text wrapping
+        this.getChildren().add(title); // add textlabel to recipe
+
+        details = new TextArea(); 
+        details.setPrefSize(475, 400); // set size of text field
         details.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0;"); // set background color of texfield
         details.setPadding(new Insets(10, 0, 10, 0)); // adds some padding to the text field
+        details.setWrapText(true); // Enable text wrapping
+        details.prefWidthProperty().bind(this.widthProperty()); // TextArea matches window width
+        details.prefHeightProperty().bind(this.heightProperty()); // TextArea matches window height
         this.getChildren().add(details); // add textlabel to recipe
+    }
+
+    public TextArea getDetails() {
+        return this.details;
+    }
+
+    public TextArea getTitle() {
+        return this.title;
     }
 
     public void setDetails(String detailString){
         details.setText(detailString);
-    }
-
-    public TextField getDetails() {
-        return this.details;
     }
 }
