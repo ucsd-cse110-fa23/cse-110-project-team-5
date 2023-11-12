84ae0e2d04877a90b0a7efba6a4c62998a475cca
DaikonPlays <kevinyan904@gmail.com>
2023-11-11 18:37:33 -0800

    added comments to Recipegenerate

--- src/main/java/client/RecipeGenerate.java
+++ src/main/java/client/RecipeGenerate.java
@@ -38,25 +38,44 @@ public class RecipeGenerate extends BorderPane {
         isRecording = !isRecording;
         return isRecording;
     }
-
+    /**
+     * Initializes and starts the audio recording process.
+     * This method sets up audio recording as well as creates the file to write to
+     * and starts a new thread to handle the audio input stream. The input stream is
+     * written to "voiceinstructions.wav".
+     * The label on the UI is also updated to indicate that recording has started.
+     */
     public void startAudioRecording() {
         try {
+            // Define the audio format with sample rate of 44100 Hz, 16 bits per sample,
+            // mono, signed audio with little-endian byte order
             AudioFormat format = new AudioFormat(44100, 16, 1, true, false);
+            // Set up the DataLine information with the specified audio format
             DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            // Get and open the target data line for capturing audio
             targetLine = (TargetDataLine) AudioSystem.getLine(info);
             targetLine.open(format);
+            // Start capturing audio data
             targetLine.start();
+            // Make the recording label visible on the UI
             recordingLabel.setVisible(true);
 
+            // Create the output file where the audio data will be saved
             outputFile = new File("voiceinstructions.wav");
+
+            // Create and start a new thread to write the audio data to a file
             Thread recordThread = new Thread(() -> {
                 try {
+                    // Write the audio input stream to the output file in WAVE file format
                     AudioSystem.write(new AudioInputStream(targetLine), AudioFileFormat.Type.WAVE, outputFile);
                 } catch (Throwable e) {
                     e.printStackTrace();
                 }
             });
+
+            // Set the thread as a daemon so it does not prevent the application from exiting
             recordThread.setDaemon(true);
+            // Start the recording thread
             recordThread.start();
         } catch (Throwable e) {
             e.printStackTrace();
@@ -68,12 +87,22 @@ public class RecipeGenerate extends BorderPane {
         targetLine.close();
         recordingLabel.setVisible(false);
     }
+    /**
+     * Retrieves the response from user voice command by recording voice input to an audio file.
+     * Method sends a request to the 'whisper' API to process the 'voiceinstructions.wav' file, 
+     * while simultaneously determines the meal type. 
+     *
+     * @return The voice input as a String from the 'Whisper' API, with spaces replaced
+     * by underscores.
+     */
     public String retrieveVoiceCommandResponse() {
         Model model = new Model();
         String mod = "";
         try {
+            // Perform a GET request to the 'whisper' endpoint with the audio file
             whisperResponse = model.performRequest("GET", "whisper", "voiceinstructions.wav");
             String mealTypecheck = whisperResponse.toLowerCase();
+            // Determine the meal type based on the response content
             if(mealTypecheck.contains("breakfast")) {
                 mealType = "breakfast";
             }
@@ -83,6 +112,7 @@ public class RecipeGenerate extends BorderPane {
             else if(mealTypecheck.contains("dinner")) {
                 mealType = "dinner";
             }
+            // Replace spaces with underscores for subsequent API request formatting
             mod = whisperResponse.replaceAll(" ", "_");
         }
         catch (Exception e) {
@@ -90,16 +120,27 @@ public class RecipeGenerate extends BorderPane {
         }
         return mod;
     }
+
+    /**
+     * Generates a recipe based on the voice input from the user by
+     * passing voice input to the 'GPT' API, which generates a recipe.
+     * The meal type and ingredients from the voice command response
+     * are included in the request.
+     *
+     * @return The recipe generated from the 'GPT' API, or an empty string
+     * if an exception occurs.
+     */
     public String fetchGeneratedRecipe() {
         Model model = new Model();
         String gptResponse = "";
         try {
+            // Retrieve ingredients from the voice command response
             String ingredients = retrieveVoiceCommandResponse();
+            // Construct and perform a GET request to the 'gpt' endpoint with the necessary parameters
             gptResponse = model.performRequest("GET", "gpt", "500," + recipeIntro + mealType + recipeIntro2 + ingredients);
         } catch (Exception e) {
             System.out.println("No input detected");
         }
         return gptResponse;
     }
-
 }
