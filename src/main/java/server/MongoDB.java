f8274652cc653ecd582cd9f01dfb2244a3736aa0
rsa1to <ryutechncode@gmail.com>
2023-11-24 15:26:28 -0800

    #91-added settings.gradle for mongodb setup

---
+++ src/main/java/server/MongoDB.java
@@ -0,0 +1,18 @@
+package server;
+
+import static com.mongodb.client.model.Filters.eq;
+
+
+import org.bson.Document;
+
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
+
+public class MongoDB {
+    
+}
