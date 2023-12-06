0574b179e8328a3dba1af2ec90c89c584dd2769a
Evan Zhou <evanzhou413@gmail.com>
2023-12-05 17:30:47 -0800

    add cleanup to scenario tests

--- src/test/java/scenario/ScenarioTests.java
+++ src/test/java/scenario/ScenarioTests.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static com.mongodb.client.model.Filters.eq;
 
 import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -15,7 +17,7 @@ import com.mongodb.client.MongoDatabase;
 import java.util.*;
 
 import server.MongoDB;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ScenarioTests {
 
     String uri = "mongodb://rsaito:Nimono8871@ac-7nibm9a-shard-00-00.idfww8h.mongodb.net:27017,ac-7nibm9a-shard-00-01.idfww8h.mongodb.net:27017,ac-7nibm9a-shard-00-02.idfww8h.mongodb.net:27017/?ssl=true&replicaSet=atlas-12jat1-shard-0&authSource=admin&retryWrites=true&w=majority";
@@ -70,4 +72,12 @@ public class ScenarioTests {
         }
     }
     
+    @AfterAll
+    void cleanup() {
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase sampleTrainingDB = mongoClient.getDatabase("Pantry_Pal");
+            MongoCollection<Document> usersCollection = sampleTrainingDB.getCollection("Users");
+            usersCollection.deleteOne(eq("username", "Mike Ross"));
+        } catch (Exception e) {}
+    }
 }
