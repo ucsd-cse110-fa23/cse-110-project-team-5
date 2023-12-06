b40bebccb7274c6d9b94a0b161877c0e10ef6241
ryus <125172564+rsa1to@users.noreply.github.com>
2023-12-05 19:52:44 -0800

    #115 - DS4 Final Improvements To UI (#139)
    * #116 Added color and changed button style in recording
    
    * #116 Changed fonts and added button background
    
    * #124 UI
    
    * #125 Recipe Details UI
    
    * #133 Login Screen UI
    
    * #124 Recipe List UI
    
    * #124 Recipe List UI
    
    * #116 UI changes
    
    * #116-sign up screen bug fixes
    
    * #115 DS4 error and bug fixing
    
    * #115-finalize
    
    * updating README
    
    * Update README.md
    
    * fixing mockUser
    
    * Update README.md
    
    ---------
    
    Co-authored-by: DaikonPlays <kevinyan904@gmail.com>
    Co-authored-by: ajhahn <ajhahn@ucsd.edu>
    Co-authored-by: Nowcarpediem <dchu@ucsd.edu>
    Co-authored-by: thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>

--- src/test/java/mock/MockUser.java
+++ src/test/java/mock/MockUser.java
@@ -7,45 +7,46 @@ import client.User;
 public class MockUser {
     String mockusername;
     String mockpassword;
-    private static final Preferences prefs = Preferences.userNodeForPackage(User.class);
+    private static final Preferences mockprefs = Preferences.userNodeForPackage(User.class);
 
-    public MockUser(String mockusername, String mockpassword){
+    public MockUser(String mockusername, String mockpassword) {
         this.mockusername = mockusername;
         this.mockpassword = mockpassword;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return mockusername;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return mockpassword;
     }
 
     // Save user login state
     public void saveLoginState(boolean rememberMe) {
-        prefs.putBoolean("REMEMBER_ME", rememberMe);
+        mockprefs.putBoolean("REMEMBER_ME", rememberMe);
         if (rememberMe) {
-            prefs.put("USERNAME", mockusername);
-            prefs.put("PASSWORD", mockpassword);
+            mockprefs.put("USERNAME", mockusername);
+            mockprefs.put("PASSWORD", mockpassword);
         } else {
-            prefs.remove("USERNAME");
-            prefs.remove("PASSWORD");
+            mockprefs.remove("USERNAME");
+            mockprefs.remove("PASSWORD");
         }
     }
 
     // Check if the user has chosen to be remembered
     public boolean isRemembered() {
-        return prefs.getBoolean("REMEMBER_ME", false);
+        return mockprefs.getBoolean("REMEMBER_ME", false);
     }
 
     // Retrieve saved username
     public String getSavedUsername() {
-        return prefs.get("USERNAME", "");
+        return mockprefs.get("USERNAME", "");
     }
 
-    // Retrieve saved password (not recommended for real applications due to security concerns)
+    // Retrieve saved password (not recommended for real applications due to
+    // security concerns)
     public String getSavedPassword() {
-        return prefs.get("PASSWORD", "");
+        return mockprefs.get("PASSWORD", "");
     }
 }
